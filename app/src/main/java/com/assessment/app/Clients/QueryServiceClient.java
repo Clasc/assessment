d9c052145d981d7daaa2f8dc6c31613eb5076969
Christian Lascsak <christian.lascsak7@gmail.com>
2021-10-03 15:16:45 +0200

    (c) move hardcoded values to env file

--- app/src/main/java/com/assessment/app/Clients/QueryServiceClient.java
+++ app/src/main/java/com/assessment/app/Clients/QueryServiceClient.java
@@ -3,6 +3,7 @@ package com.assessment.app.Clients;
 import com.assessment.app.Models.PostCall;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -13,7 +14,13 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class QueryServiceClient {
-    private String host = "http://query-service:8002";
+
+    @Value("${query.service.hostname}")
+    private String host;
+
+    @Value("${query.service.port}")
+    private String port;
+
     private RestTemplate rest;
     private HttpHeaders headers;
     private HttpStatus status;
@@ -26,18 +33,8 @@ public class QueryServiceClient {
         headers.add("Accept", "*/*");
     }
 
-    public String get(String uri) {
-        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
-        ResponseEntity<String> responseEntity = rest.exchange(host + uri, HttpMethod.GET, requestEntity, String.class);
-        this.setStatus(responseEntity.getStatusCode());
-        return responseEntity.getBody();
-    }
-
-    public String post(String uri, PostCall request) {
-        HttpEntity<PostCall> requestEntity = new HttpEntity<PostCall>(request, headers);
-        ResponseEntity<String> responseEntity = rest.exchange(host + uri, HttpMethod.POST, requestEntity, String.class);
-        this.setStatus(responseEntity.getStatusCode());
-        return responseEntity.getBody();
+    public String getBaseUrl() {
+        return host + ":" + port;
     }
 
     public HttpStatus getStatus() {
@@ -47,4 +44,21 @@ public class QueryServiceClient {
     public void setStatus(HttpStatus status) {
         this.status = status;
     }
+
+    public String get(String uri) {
+        HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
+        ResponseEntity<String> responseEntity = rest.exchange(getBaseUrl() + uri, HttpMethod.GET, requestEntity,
+                String.class);
+        this.setStatus(responseEntity.getStatusCode());
+        return responseEntity.getBody();
+    }
+
+    public String post(String uri, PostCall request) {
+        HttpEntity<PostCall> requestEntity = new HttpEntity<PostCall>(request, headers);
+        ResponseEntity<String> responseEntity = rest.exchange(getBaseUrl() + uri, HttpMethod.POST, requestEntity,
+                String.class);
+        this.setStatus(responseEntity.getStatusCode());
+        return responseEntity.getBody();
+    }
+
 }
